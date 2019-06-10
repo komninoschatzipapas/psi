99d89232709dd91d7d497ae44a971046a45ac5f3
komninoschat <komninoschat@gmail.com>
2019-06-10 07:55:14 +0300

    Fix SymbolBuilder

--- modules/semantic-analyzer/src/SymbolBuilder.ts
+++ modules/semantic-analyzer/src/SymbolBuilder.ts
@@ -66,75 +66,75 @@ export default class SymbolBuilder extends AST.ASTVisitor {
   }
 
   public visitAssignment(node: AST.AssignmentAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitEmpty(node: AST.EmptyAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitInteger(node: AST.IntegerAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitIntegerConstant(node: AST.IntegerConstantAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitIntegerDivision(node: AST.IntegerDivisionAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitMinus(node: AST.MinusAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitMultiplication(node: AST.MultiplicationAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitPlus(node: AST.PlusAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitReal(node: AST.RealAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitRealConstant(node: AST.RealConstantAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitRealDivision(node: AST.RealDivisionAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitType(node: AST.TypeAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitUnaryMinus(node: AST.UnaryMinusAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitUnaryPlus(node: AST.UnaryPlusAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitMod(node: AST.ModAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitBoolean(node: AST.BooleanAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitTrue(node: AST.TrueAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitFalse(node: AST.FalseAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitEquals(node: AST.EqualsAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitNotEquals(node: AST.NotEqualsAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitGreaterThan(node: AST.GreaterThanAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitLessThan(node: AST.LessThanAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitGreaterEquals(node: AST.GreaterEqualsAST): void {
-
+    node.children.forEach(this.visit.bind(this));
   }
   public visitLessEquals(node: AST.LessEqualsAST): void {
-    
+    node.children.forEach(this.visit.bind(this));
   }
 }
